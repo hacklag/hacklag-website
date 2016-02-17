4d1c6df09a1e1965fcbc026fab28fe2ae1d74048
CI <karol.kowalski@eyedea.io>
2016-02-17 16:41:44 +0100

    [HAC-76] Added 3 component states, based on invitation status response from Syncano

--- src/apps/Pages/Community/Slack.jsx
+++ src/apps/Pages/Community/Slack.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {TextField, RaisedButton} from 'material-ui';
 import Syncano from 'syncano';
-import Router from 'react-router';
 
 export default React.createClass({
   displayName: 'Slack',
 
   getInitialState() {
     return {
-      textFieldValue: ''
+      textFieldValue: '',
+      registerState: null
     };
   },
 
@@ -25,44 +25,66 @@ export default React.createClass({
     };
   },
 
-  handleTextFieldChange(e) {
+  handleTextFieldChange(event) {
     this.setState({
-      textFieldValue: e.target.value
+      textFieldValue: event.target.value
     });
   },
 
   handleButtonPress() {
     const {Webhook} = Syncano({accountKey: SYNCANO_API_KEY});
     const SLACK_PUBLIC_WEBHOOK = '391f2e566770e4350451464fbaca51ef35865994';
-    let payload = '{email: "$this.state.textFieldValue"}';
+    let payload = JSON.stringify({email: this.state.textFieldValue});
 
     Webhook.please().runPublic({
       instanceName: SYNCANO_INSTANCE_NAME,
       public_link: SLACK_PUBLIC_WEBHOOK},
       {payload}
     ).then((trace) => {
-      console.log(trace);
+      let data = JSON.parse(trace.result.stdout);
+
+      this.setState({registerState: data.success});
     });
   },
 
   render() {
     let styles = this.getStyles();
+    let content = '';
+
+    if (this.state.registerState === null) {
+      content = (
+        <div>
+          <h5>Sign up today and be part of still growing community!</h5>
+          <TextField
+            floatingLabelText='Email address'
+            value={this.state.textFieldValue}
+            type='email'
+            onChange={this.handleTextFieldChange}
+          /><br/>
+          <RaisedButton
+            style={styles.inviteButton}
+            label='Get an invite'
+            onClick={this.handleButtonPress}
+          />
+        </div>);
+    } else if (this.state.registerState === true) {
+      content = (
+        <div>
+          <h5>Thank you for signing up!</h5>
+          Please check your inbox to find invitation for Slack and Forum.
+        </div>);
+    } else if (this.state.registerState === false) {
+      content = (
+        <div>
+          <h5>Something went wrong!</h5>
+          There was a problem with registation, please contact support at support@eyedea.io
+        </div>);
+    }
 
     return (
       <div style={styles.componentBody}>
-        Sign up today and be part of still growing community!<br/>
-        <TextField
-          floatingLabelText='Email address'
-          value={this.state.textFieldValue}
-          type='email'
-          onChange={this.handleTextFieldChange}
-        /><br/>
-        <RaisedButton
-          style={styles.inviteButton}
-          label='Get an invite'
-          onClick={this.handleButtonPress}
-        />
+        {content}
       </div>
-    );
+      );
   }
 });
