07e3e270473c717bc371b301028d121bb601808d
CI <karol.kowalski@eyedea.io>
2016-02-17 20:49:01 +0100

    [HAC-151] Style improvement, added HackBat dialog box

--- src/apps/LandingRegister/LandingRegister.react.js
+++ src/apps/LandingRegister/LandingRegister.react.js
@@ -1,43 +1,60 @@
 import React from 'react';
 import Radium from 'radium';
+import {TextField, RaisedButton} from 'material-ui';
 
 export default Radium(React.createClass({
   displayName: 'LandingRegister',
+
+  getInitialState() {
+    return {
+      textFieldValue: ''
+    };
+  },
 
   getStyles() {
     return {
       componentBody: {
         color: '5d5d5d',
-        backgroundColor: '#fafafa',
-        backgroundImage: 'url(/img/background_sendemail.png)',
-        backgroundSize: 'cover',
+        //backgroundColor: '#fafafa',
         display: 'flex',
         flexDirection: 'row',
-        fontSize: 24
+        fontSize: 24,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 184
+      },
+      develBackgroundImage: {
+        backgroundImage: 'url(/img/background_sendemail.png)',
+        backgroundSize: '100%'
       },
       logoBar: {
         border: '1px solid red',
         display: 'flex',
         flexDirection: 'column',
-        width: 324
+        maxWidth: 324
       },
       contentBar: {
-        border: '1px dashed green',
+        border: '1px solid green',
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        width: 554
       },
       hackLagLogo: {
-        width: 324,
-        height: 314
+        width: 'auto',
+        maxHeight: 392
       },
       socialBar: {
         flexDirection: 'row',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginTop: 24
       },
       socialLogo: {
         width: 64,
         height: 64
+      },
+      inviteButton: {
+        backgroundColor: '#ec018c'
       }
     };
   },
@@ -46,16 +63,34 @@ export default Radium(React.createClass({
     let styles = this.getStyles();
 
     return (
-      <div style={styles.componentBody}>
-        <div style={styles.logoBar}>
-          <img style={styles.hackLagLogo} src={"/img/hacklag_sketch.png"} alt="Hacklag logo" />
-          <div style={styles.socialBar}>
-            <img style={styles.socialLogo} src={"/img/icons/discourse.png"} alt="Discourse logo" />
-            <img style={styles.socialLogo} src={"/img/icons/slack.png"} alt="Slack logo" />
+      <div style={styles.develBackgroundImage}>
+        <div style={styles.componentBody}>
+          <div style={styles.logoBar}>
+            <img style={styles.hackLagLogo} src={"/img/hacklag_sketch.png"} alt="Hacklag logo" />
+            <div style={styles.socialBar}>
+              <img style={styles.socialLogo} src={"/img/icons/discourse.png"} alt="Discourse logo" />
+              <img style={styles.socialLogo} src={"/img/icons/slack.png"} alt="Slack logo" />
+            </div>
           </div>
-        </div>
-        <div style={styles.contentBar}>
-          Main text with register button
+          <div style={styles.contentBar}>
+            Hacklag is the name of the first Hackerspace in Białystok.<br/><br/>
+            To be part of this initiative or just be up to date with the progress
+            please type your email and we will send you invitation to our
+            forum and chat.
+            Demonstrating size and engagement of the community is
+            extremly important to gather founds and make this thing real -
+            please sign in!
+
+            <TextField
+              floatingLabelText='Your Email'
+              value={this.state.textFieldValue}
+              type='email'
+            /><br/>
+            <RaisedButton
+              style={styles.inviteButton}
+              label='Cool, let me in!'
+            />
+          </div>
         </div>
       </div>
     );
