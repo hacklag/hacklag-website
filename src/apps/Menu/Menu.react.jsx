c622e520c278aca26e67cbba2cd21dec08625a86
kkotelczuk <konrad.kotelczuk@gmail.com>
2016-02-20 02:39:24 +0100

    [HAC-89] Added responsive menu based on flexboxgrid, without any styling

--- src/apps/Menu/Menu.react.jsx
+++ src/apps/Menu/Menu.react.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import Grommet from 'grommet';
 import Radium from 'radium';
+import {Grid, Row, Col} from 'react-flexbox-grid';
 
 export default Radium(React.createClass({
   displayName: 'Menu',
+
+  getInitialState() {
+    return {
+      toogle: false
+    };
+  },
 
   getStyles() {
     return {
@@ -16,28 +22,56 @@ export default Radium(React.createClass({
           color: '#38B7EA',
           textDecoration: 'none'
         }
+      },
+      menu: {
+        '@media (max-width: 48em)': {
+          display: 'none'
+        }
+      },
+      menuSlideDown: {
+        display: 'block'
+      },
+      toogle: {
+        display: 'none',
+        '@media (max-width: 48em)': {
+          display: 'block'
+        }
+      },
+      menuItem: {
+        textAlign: 'center'
       }
     };
+  },
+
+  handleClick(event) {
+    this.setState({toggle: !this.state.toggle});
   },
 
   render() {
     let styles = this.getStyles();
 
     return (
-      <Grommet.Menu direction="row" size="large" dropAlign={{right: 'right'}}>
-        <Grommet.Anchor href="/#/home">
-          <span key="home" style={styles.menuText}>Home</span>
-        </Grommet.Anchor>
-        <Grommet.Anchor href="/#/events">
-          <span key="events" style={styles.menuText}>Events</span>
-        </Grommet.Anchor>
-        <Grommet.Anchor href="/#/blog">
-          <span key="blog" style={styles.menuText}>Blog</span>
-        </Grommet.Anchor>
-        <Grommet.Anchor href="/#/about">
-          <span key="about" style={styles.menuText}>About</span>
-        </Grommet.Anchor>
-      </Grommet.Menu>
+      <div>
+        <div key="menu" style={this.state.toggle ? styles.menuSlideDown : styles.menu}>
+          <Grid >
+            <Row end={'sm'} start={'xs'}>
+              <Col xs={12} sm={2} md={1} style={styles.menuItem}>
+                <a href="/#/home" key="home" style={styles.menuText}>Home</a>
+              </Col>
+              <Col xs={12} sm={2} md={1} style={styles.menuItem}>
+                <a href="/#/events" key="events" style={styles.menuText}>Events</a>
+              </Col>
+              <Col xs={12} sm={2} md={1} style={styles.menuItem}>
+                <a href="/#/blog" key="blog" style={styles.menuText}>Blog</a>
+              </Col>
+              <Col xs={12} sm={2} md={1} style={styles.menuItem}>
+                <a href="/#/about" key="about" style={styles.menuText}>About</a>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
+        <div key="toogle" style={styles.toogle} onClick={this.handleClick}>toggle</div>
+      </div>
     );
   }
 }));
