9d78c2a5b34cf9e4edf520576813d2911f16a464
kkotelczuk <konrad.kotelczuk@gmail.com>
2016-02-20 02:55:53 +0100

    [HAC-89] Added styling for toggled menu

--- src/apps/Menu/Menu.react.jsx
+++ src/apps/Menu/Menu.react.jsx
@@ -7,7 +7,7 @@ export default Radium(React.createClass({
 
   getInitialState() {
     return {
-      toogle: false
+      toggle: false
     };
   },
 
@@ -29,9 +29,14 @@ export default Radium(React.createClass({
         }
       },
       menuSlideDown: {
-        display: 'block'
+        display: 'block',
+        top: '80px',
+        left: 0,
+        position: 'absolute',
+        backgroundColor: '#FFFFFF',
+        width: '100%'
       },
-      toogle: {
+      toggle: {
         display: 'none',
         '@media (max-width: 48em)': {
           display: 'block'
@@ -39,6 +44,11 @@ export default Radium(React.createClass({
       },
       menuItem: {
         textAlign: 'center'
+      },
+      menuItemSlideDown: {
+        textAlign: 'left',
+        paddingLeft: '24px'
+
       }
     };
   },
@@ -55,22 +65,22 @@ export default Radium(React.createClass({
         <div key="menu" style={this.state.toggle ? styles.menuSlideDown : styles.menu}>
           <Grid >
             <Row end={'sm'} start={'xs'}>
-              <Col xs={12} sm={2} md={1} style={styles.menuItem}>
+              <Col xs={12} sm={2} md={1} style={this.state.toggle ? styles.menuItemSlideDown : styles.menuItem}>
                 <a href="/#/home" key="home" style={styles.menuText}>Home</a>
               </Col>
-              <Col xs={12} sm={2} md={1} style={styles.menuItem}>
+              <Col xs={12} sm={2} md={1} style={this.state.toggle ? styles.menuItemSlideDown : styles.menuItem}>
                 <a href="/#/events" key="events" style={styles.menuText}>Events</a>
               </Col>
-              <Col xs={12} sm={2} md={1} style={styles.menuItem}>
+              <Col xs={12} sm={2} md={1} style={this.state.toggle ? styles.menuItemSlideDown : styles.menuItem}>
                 <a href="/#/blog" key="blog" style={styles.menuText}>Blog</a>
               </Col>
-              <Col xs={12} sm={2} md={1} style={styles.menuItem}>
+              <Col xs={12} sm={2} md={1} style={this.state.toggle ? styles.menuItemSlideDown : styles.menuItem}>
                 <a href="/#/about" key="about" style={styles.menuText}>About</a>
               </Col>
             </Row>
           </Grid>
         </div>
-        <div key="toogle" style={styles.toogle} onClick={this.handleClick}>toggle</div>
+        <div key="toggle" style={styles.toggle} onClick={this.handleClick}>toggle</div>
       </div>
     );
   }
