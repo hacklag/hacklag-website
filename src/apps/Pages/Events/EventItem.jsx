24da8f63abd8659734abca1d9edc3a7e51cf044d
Dawid Kozak <d.kozak92@gmail.com>
2016-02-25 17:55:55 +0100

    [HAC-63] Clean up

--- src/apps/Pages/Events/EventItem.jsx
+++ src/apps/Pages/Events/EventItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardHeader, CardTitle, CardText, CardActions, FlatButton} from 'material-ui';
+import {Card, CardTitle, CardText} from 'material-ui';
 
 export default ({event}) => {
   const styles = {
