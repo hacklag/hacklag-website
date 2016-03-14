3125d0349cbaca696c0bb645809296de42ce48e4
kkkowalski <karol.kowalski@eyedea.io>
2016-03-14 17:59:12 +0100

    [HAC-201] Fixed LandingForm.js

--- src/apps/LandingRegister/LandingForm.js
+++ src/apps/LandingRegister/LandingForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Radium from 'radium';
 import {Form} from 'formsy-react';
 import {FormsyText} from 'formsy-material-ui';
-import {RaisedButton} from 'material-ui';
+import {RaisedButton, CircularProgress} from 'material-ui';
 import Syncano from 'syncano';
 
 export default Radium(React.createClass({
@@ -12,18 +12,34 @@ export default Radium(React.createClass({
 
   getInitialState() {
     return {
-      canSubmit: false
+      canSubmit: false,
+      progressVisible: false
     };
   },
 
   getStyles() {
     return {
       inviteButton: {
-        width: 190,
-        marginBottom: 24
+        width: 210,
+        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.26), 0 2px 10px rgba(0, 0, 0, 0.16)'
       },
       buttonLabelStyle: {
         fontSize: 16
+      },
+      progressCircleVisible: {
+        display: 'flex',
+        margin: -7
+      },
+      progressCircleHidden: {
+        display: 'none',
+        margin: -7
+      },
+      submitBox: {
+        display: 'flex',
+        marginBottom: 24
+      },
+      formInputStyle: {
+        marginBottom: 24
       }
     };
   },
@@ -32,6 +48,8 @@ export default Radium(React.createClass({
     const {Webhook} = Syncano({accountKey: SYNCANO_API_KEY});
     const COMMUNITY_REGISTER_WEBHOOK = '95f37de28a7f95f07b2128677e97d116fcf8813b';
     const payload = JSON.stringify({email: model.email});
+
+    this.setState({progressVisible: true});
 
     Webhook.please().runPublic({
       instanceName: SYNCANO_INSTANCE_NAME,
@@ -46,6 +64,7 @@ export default Radium(React.createClass({
 
   render() {
     const styles = this.getStyles();
+    let {progressVisible} = this.state;
 
     return (
       <Form
@@ -60,14 +79,20 @@ export default Radium(React.createClass({
           validationError="You have to type valid email"
           floatingLabelText="Your Email"
           required={true}
+          style={styles.formInputStyle}
           fullWidth={true} />
-        <RaisedButton
-          style={styles.inviteButton}
-          labelStyle={styles.buttonLabelStyle}
-          primary={true}
-          label="Cool, let me in!"
-          onClick={() => this.refs.form.validateForm()}
-          type="submit" />
+        <div style={styles.submitBox}>
+          <RaisedButton
+            style={styles.inviteButton}
+            labelStyle={styles.buttonLabelStyle}
+            primary={true}
+            label="I want to join!"
+            onClick={() => this.refs.form.validateForm()}
+            type="submit" />
+          <CircularProgress
+            style={progressVisible ? styles.progressCircleVisible : styles.progressCircleHidden}
+            size={0.4} />
+        </div>
       </Form>
     );
   }
