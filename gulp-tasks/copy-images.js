8519cb04134208979138b8022132d4c624e528a9
Dawid Kozak <d.kozak92@gmail.com>
2016-03-29 12:44:10 +0200

    Image minification

--- gulp-tasks/copy-images.js
+++ gulp-tasks/copy-images.js
@@ -1,8 +1,10 @@
 var paths = require('./_common')().paths,
-    gulp = require('gulp');
+  gulp = require('gulp'),
+  imagemin = require('gulp-imagemin');
 
 module.exports = function() {
   return gulp.src(paths.images)
+  .pipe(imagemin({progressive: true}))
   .pipe(gulp.dest('dist/img'));
 };
 
