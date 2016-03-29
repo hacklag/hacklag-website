4fb2b328f5ce5d1f9a8cdac3f79b7690ec31d45b
Dawid Kozak <d.kozak92@gmail.com>
2016-03-29 14:12:53 +0200

    Image minification - add png

--- gulp-tasks/copy-images.js
+++ gulp-tasks/copy-images.js
@@ -1,10 +1,15 @@
 var paths = require('./_common')().paths,
   gulp = require('gulp'),
-  imagemin = require('gulp-imagemin');
+  imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant');
 
 module.exports = function() {
   return gulp.src(paths.images)
-  .pipe(imagemin({progressive: true}))
+  .pipe(imagemin({
+    optimizationLevel: 3,
+    progressive: true,
+    use: [pngquant()]
+  }))
   .pipe(gulp.dest('dist/img'));
 };
 
