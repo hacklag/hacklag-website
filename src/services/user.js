7e5950bb3352efddcee684ae6937ebcbcb5cf6aa
Kamil Antropik <kamil.antropik@eyedea.io>
2017-03-22 18:49:09 +0100

    feat(login-register): handle register. Add long poll to verify user click

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,8 @@
 import { action, request } from 'utils';
 
 const COMMUNITY_REGISTER_URL = `https://api.syncano.io/v1.1/instances/${SYNCANO_INSTANCE_NAME}/endpoints/scripts/p/aab6325ffceb4f324ddc6370fa79a6d4ffdf2280/communityregistration_endpoint/?template=script_json`;
-
+const LOGIN_REGISTER_URL = 'https://silent-field-9988.syncano.space/magiclink/login/';
+const VERIFY_USER_URL = 'https://silent-field-9988.syncano.space/magiclink/verify/';
 export default class User {
   @action signup = async ({ email }) => {
     this.store.pending.set('user.signup', { status: true });
@@ -13,5 +14,48 @@ export default class User {
     });
 
     this.store.pending.set('user.signup', { status: false });
+  }
+  pollVerify = (email) => {
+    const form = new FormData();
+    form.append('email', email);
+
+    const promise = setInterval(() => {
+      fetch(VERIFY_USER_URL, {
+        method: 'post',
+        body: form,
+      }).then(res => {
+        res.json()
+          .then(data => {
+            if (data.status) {
+              console.log(data.userKey);
+              clearInterval(promise);
+            }
+          });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    }, 4000);
+    // this.store.userKey = userKey;
+    // this.store.messages.set('user.pollverified', {
+    //   status: status === 'true' ? window.location.assign('/login') : 'Pending',
+    // });
+    // this.store.pending.set('user.register', { status: false });
+  }
+  @action register = async (form) => {
+    const { email, firstname, lastname, isVolunteer } = form;
+    const { body, status } = await request.post(LOGIN_REGISTER_URL, {
+      email,
+      firstname,
+      lastname,
+      isVolunteer,
+    });
+    console.log(email);
+    this.pollVerify(email);
+    this.store.messages.set('user.register', {
+      status: status === 'true' ? 'success' : 'failure',
+      message: status === 'true' ? 'You have been registered, chcek your email' : body,
+    });
+    this.store.pending.set('user.register', { status: false });
   }
 }
--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,15 +1,40 @@
 import React, { PropTypes } from 'react';
-import { Wrapper, Join, Button } from 'bits';
+import { Wrapper, Button } from 'bits';
 import { Header, Footer } from 'bytes';
 import { connect } from 'utils';
 import styles from './styles.css';
 
 const cn = require('classnames/bind').bind(styles);
 
-const Landing = () => {
+const Landing = ({
+  store: {
+    messages, pending,
+  },
+  services: {
+    user: { register },
+  },
+}) => {
+  const messageRegister = messages.get('user.register');
+  const isLoading = pending.get('user.register');
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const selectNodeValue = nodeNumber => e.target.childNodes[nodeNumber];
+    const clearNodeValues = () => {
+      e.target.childNodes.forEach(node => {
+        const nodec = node;
+        nodec.value = '';
+      });
+      selectNodeValue(7).checked = 'false';
+    };
+    const form = {
+      firstname: selectNodeValue(1).value,
+      lastname: selectNodeValue(3).value,
+      email: selectNodeValue(5).value,
+      isVolunteer: selectNodeValue(7).checked,
+    };
+    register(form);
+    clearNodeValues();
   };
   return (
     <div className={cn('Landing')}>
@@ -27,25 +52,27 @@ const Landing = () => {
                   <label htmlFor={'lastname'}>Lastname</label>
                   <input type={'text'} name={'lastname'} placeholder={'Lastname'} className={cn('Register--form_lastname')} />
 
-                  <label htmlFor={'login'}>Login</label>
-                  <input type={'text'} name={'login'} placeholder={'Login'} className={cn('Register--form_login')} />
-
-                  <label htmlFor={'passowrd'}>Password</label>
-                  <input type={'password'} name={'password'} placeholder={'Password'} className={cn('Register--form_password')} />
-
                   <label htmlFor={'email'}>Email</label>
                   <input type={'email'} name={'email'} placeholder={'jon@doe.com'} className={cn('Register--form_email')} />
 
                   <label htmlFor={'Volunteer'}>I want to be a volunteer</label>
-                  <input type={'checkbox'} name={'volunteer'} placeholder={'I want to be a volunteer'} className={cn('Register--form_volunteer')} />
-
-                  <Button type={'submit'} name={'submit'} className={'primary'}>Submit</Button>
+                  <input type={'checkbox'} name={'volunteer'} value="off" className={cn('Register--form_volunteer')} />
+                  <Button type={'submit'} name={'submit'} className={'btn-primary'} disabled={isLoading ? isLoading.status : false}>Submit</Button>
+                  {messageRegister && (
+                    <div>
+                      <p
+                        className={cn('u-mt- u-ta-c', {
+                          isSuccess: messageRegister.status === 'success',
+                          isFailure: messageRegister.status === 'failure',
+                        })}
+                      >
+                      {messageRegister.message}
+                      </p>
+                    </div>
+                )}
                 </form>
               </div>
             </div>
-            <Join className="u-ta-c" spacing="sm">
-              <a className="Btn Btn--primary" href="mailto:sponsorship@hacklag.org">Join</a>
-            </Join>
           </div>
         </Wrapper>
       </div>
